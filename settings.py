081dca328d6b5c094aeca1b8c212930a9dbc0666
DILEEP P D <dileeppd45@gmail.com>
2023-10-25 20:23:39 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'NAME': 'aghosh_charity_service',
         'ENGINE': 'django.db.backends.mysql',
-        'USER': 'aghosh',
-        'PASSWORD': 'farm2020',
+        'USER': '####',
+        'PASSWORD': '####',
         'HOST': 'master.herosite.pro',
         'PORT': '3306',
         'OPTIONS': {
